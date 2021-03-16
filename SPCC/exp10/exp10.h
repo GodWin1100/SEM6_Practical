fcf2f370a601b74e65354c3af750c346df0c9f81
GodWin <shivamjpanchal1@gmail.com>
2021-03-17 00:16:34 +0530

    added prev commit

---
+++ SPCC/exp10/exp10.h
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void add(int a,int b){
+    printf("Added Value=%d\n", a+b);
+}
+
+void sub(int a,int b){
+    printf("Subtracted Value=%d\n", a-b);
+}
+
+void mul(int a,int b){
+    printf("Multiplied Value=%d\n", a*b);
+}
+
+void div(int a,int b){
+    printf("Divided Value=%d\n", a/b);
+}
+
+void mod(int a,int b){
+    printf("Modulus Value=%d\n", a%b);
+}
